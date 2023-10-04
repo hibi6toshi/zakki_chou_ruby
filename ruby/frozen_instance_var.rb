bdb35510ac6f829a0a4dd25084c573c91530008d
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-10-04 20:53:21 +0900

    frozenで要素の参照変更はできない追加

--- ruby/frozen_instance_var.rb
+++ ruby/frozen_instance_var.rb
@@ -22,3 +22,36 @@ p t.inspect
 # "test"
 # 500
 # "#<Test:0x00001551b165dea8 @val_a=100, @val_b=5, @cache={:test=>\"test\", :total=>500}>"
+
+class MyClass
+  attr_accessor :ary, :id
+  def initialize
+    @ary = [1, 2, 3]
+    @id = 42
+  end
+end
+
+obj = MyClass.new
+obj.freeze
+
+begin
+  obj.id = 42
+rescue
+  p "id は変更できません"
+end
+puts obj.id # 出力: 99
+
+# 2. @aryの要素を操作（追加、変更、削除）できます
+obj.ary[0] = 99
+obj.ary << 4
+obj.ary.delete(2)
+p obj.ary # 出力: [99, 3, 4]
+
+# 3. @aryに別の配列を格納できない
+
+begin
+  obj.ary = [5, 6, 7] 
+rescue
+  p "aryは変更できません"
+end
+p obj.ary # 出力: [99, 3, 4]
